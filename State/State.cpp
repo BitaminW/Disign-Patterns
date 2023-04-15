d1fa82759175e15b8550db81c35536cacf3878d8
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-04-15 23:57:49 +0900

    Update State.cpp

--- State/State.cpp
+++ State/State.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <typeinfo>
 
+class Context;  // 클래스 전방선언
+
+class State {
+protected:
+	Context* context;
+
+public:
+	virtual ~State() {}
+
+	void set_context(Context* context) {
+		this->context = context;
+	}
+
+	virtual void Handle1() = 0;
+	virtual void Handle2() = 0;
+
+};
