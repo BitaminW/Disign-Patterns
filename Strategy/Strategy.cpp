2d90949ca3252398bcce5207ac0615ed038d61d7
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-04-24 23:47:50 +0900

    Update Strategy.cpp

--- Strategy/Strategy.cpp
+++ Strategy/Strategy.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <string_view>
+
+class Strategy{
+public:
+	virtual ~Strategy() = default;
+	virtual std::string doAlogorithm(std::string_view data) const = 0;
+
+};
 
