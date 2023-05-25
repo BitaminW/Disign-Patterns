74dd876e992859b49c918d323c231e11e055ade8
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-05-25 23:54:21 +0900

    Update Visitor.cpp

--- Visitor/Visitor.cpp
+++ Visitor/Visitor.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+
+// 클래스 전방선언
+class ConcreteComponentA;
+class ConcreteComponentB;
+
+
+// interface
+class Visitor{
+public:
+	virtual void VisitConcreteComponentA(const ConcreteComponentA* element) const = 0;
+	virtual void VisitConcreteComponentB(const ConcreteComponentB* element) const = 0;
+
+};
+	
+
 
