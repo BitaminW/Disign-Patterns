80268b3a8090c1c95c26f206751235abfee79561
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-06-10 23:42:55 +0900

    Update Strategy.cpp

--- Strategy/Strategy.cpp
+++ Strategy/Strategy.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string_view>
+#include <algorithm>
 
 class Strategy{
 public:
 	virtual ~Strategy() = default;
-	virtual std::string doAlogorithm(std::string_view data) const = 0;
+	virtual std::string doAlgorithm(std::string_view data) const = 0;
 
 };
 
@@ -16,8 +17,55 @@ public:
 	explicit Context(std::unique_ptr<Strategy>&& strategy = {}) : strategy_(std::move(strategy)) {
 
 	}
-	
-	void set_strategy(std::unique_ptr<Strategy> &&strategy) {
-        strategy_ = std::move(strategy);
-    	}
+
+	void set_strategy(std::unique_ptr<Strategy>&& strategy) {
+		strategy_ = std::move(strategy);
+	}
+
+	void doSomeBusinessLogic() const {
+		if (strategy_) {
+			std::cout << "Context: Sorting data using the strategy (not sure how it'll do it)\n";
+			std::string result = strategy_->doAlgorithm("aecbd");
+			std::cout << result << "\n";
+		}
+		else {
+			std::cout << "Context: Strategy isn't set\n";
+		}
+	}
 };
+
+class ConcreteStrategyA : public Strategy
+{
+public:
+	std::string doAlgorithm(std::string_view data) const override{
+		std::string result(data);
+		std::sort(std::begin(result), std::end(result));
+
+		return result;
+	}
+};
+
+class ConcreteStrategyB : public Strategy
+{
+	std::string doAlgorithm(std::string_view data) const override{
+		std::string result(data);
+		std::sort(std::begin(result), std::end(result), std::greater<>());
+
+		return result;
+	}
+};
+
+void clientCode(){
+	Context context(std::make_unique<ConcreteStrategyA>());
+	std::cout << "Client: Strategy is set to normal sorting.\n";
+	context.doSomeBusinessLogic();
+	std::cout << "\n";
+	std::cout << "Client: Strategy is set to reverse sorting.\n";
+	context.set_strategy(std::make_unique<ConcreteStrategyB>());
+	context.doSomeBusinessLogic();
+}
+
+int main(){
+	clientCode();
+	return 0;
+}
